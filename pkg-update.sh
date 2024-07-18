42903ef21f60b7e8aa02fe4193d88fd660ea8ac8
MapleCool <37971849+pmkol@users.noreply.github.com>
2024-07-18 19:44:15 +0800

    Update pkg-update.sh

--- pkg-update.sh
+++ pkg-update.sh
@@ -5,11 +5,12 @@ bash xray-core/update.sh
 
 # sing-box
 mkdir -p sing-box/files
-curl -Ls https://raw.githubusercontent.com/openwrt/packages/master/net/sing-box/files/sing-box.conf > sing-box/files/sing-box.conf
-curl -Ls https://raw.githubusercontent.com/openwrt/packages/master/net/sing-box/files/sing-box.init > sing-box/files/sing-box.init
-curl -Ls https://raw.githubusercontent.com/openwrt/packages/master/net/sing-box/Makefile > sing-box/Makefile
+curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/files/sing-box.conf > sing-box/files/sing-box.conf
+curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/files/sing-box.init > sing-box/files/sing-box.init
+curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/Makefile > sing-box/Makefile
+cd sing-box
+ls
+cd ../
 
 # v2ray-data
 git clone https://github.com/pmkol/v2ray-geodata v2ray-geodata
-
-ls
